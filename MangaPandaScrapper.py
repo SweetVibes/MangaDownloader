bde30424c57f8a009192b1b14f76360174b33f5e
Vineet Amalkar <vbn824mm@gmail.com>
2017-06-27 22:29:03 +0530

    Fixed a bug caused getChapterNumber

--- MangaPandaScrapper.py
+++ MangaPandaScrapper.py
@@ -6,7 +6,7 @@ import time
 from urllib.request import Request
 from urllib.request import urlopen
 
-
+numberExtractor = re.compile(r"\d+")
 def getcontent(mangaurl):
     res = requests.get(mangaurl, timeout=30, headers=headers)
     res.raise_for_status()
@@ -35,7 +35,9 @@ def getSoup(text):
 
 
 def getChapterNumber(mangaInfo):
-    return int(mangaInfo["title"][-1])
+    chn = mangaInfo["title"][-3:].strip()
+    match = numberExtractor.search(chn)
+    return int(match.group())
 
 
 def downloadImage(imgURL, pageNumber):
@@ -48,18 +50,19 @@ def downloadImage(imgURL, pageNumber):
 
 def getAllImageUrls(chapterNumber, soup):
     chn = chapterNumber
-    imgURLs = []
+    print('Downloading chapter number '+str(chapterNumber))
     while chapterNumber != chn + 1:
         imgHolder = soup.find(id="imgholder")
         nextUrl = hostName + imgHolder.a['href']
         imgURL = imgHolder.img['src']
-        print(imgURL)
-        imgURLs.append(imgURL)
         nextPageContent = getcontent(nextUrl)
         soup = getSoup(nextPageContent.text)
         info = getTitleAndPage(soup)
         chapterNumber = getChapterNumber(info)
         downloadImage(imgURL, info["page"])
+        print("Downloaded "+info["page"])
+        print(info,chapterNumber)
+
 
 
 def startDownload(mangaurl):
@@ -75,6 +78,6 @@ user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/
 headers = {'User-Agent': user_agent, }
 folderPath = r"B:\Manga"
 createFolder(folderPath)
-mangaUrl = "http://www.mangapanda.com/naruto-gaiden-the-seventh-hokage/5"
+mangaUrl = "http://www.mangapanda.com/naruto-gaiden-the-seventh-hokage/10"
 hostName = "http://www.mangapanda.com"
 startDownload(mangaUrl)
